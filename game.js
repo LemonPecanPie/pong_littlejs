76c7a71eeadb2abd8054f027f87cd0853a06bcc2
Lemon Pecan Pie <aidanhpaul@gmail.com>
2025-08-24 15:44:24 -0400

    Change level size

--- game.js
+++ game.js
@@ -7,48 +7,46 @@
 'use strict';
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameInit()
-{
+
+const levelSize = vec2(20, 20);
+
+
+function gameInit() {
     // called once after the engine starts up
     // setup the game
-    const levelSize = vec2(20, 20);
-    for(let x=0; x<=20; x+=2) {
-        for(let y=0; y<=20; y+=1) {
-            const brick = new EngineObject(vec2(x,y));
+    for (let x = 2; x <= levelSize.x-2; x += 2)
+        for (let y = 12; y <= levelSize.y-2; y += 1) {
+            const brick = new EngineObject(vec2(x, y));
             brick.color = randColor();
+
         }
-    }
 
     setCameraPos(levelSize.scale(.5));
     setCanvasFixedSize(vec2(1280, 720));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameUpdate()
-{
+function gameUpdate() {
     // called every frame at 60 frames per second
     // handle input and update the game state
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameUpdatePost()
-{
+function gameUpdatePost() {
     // called after physics and objects are updated
     // setup camera and prepare for render
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameRender()
-{
+function gameRender() {
     // called before objects are rendered
     // draw any background effects that appear behind objects
-    drawRect(cameraPos, vec2(100), new Color(.5,.5,.5));
-    drawRect(cameraPos, levelSize, new Color(.1,.1,.1));
+    drawRect(cameraPos, vec2(100), new Color(.5, .5, .5));
+    drawRect(cameraPos, levelSize, new Color(.1, .1, .1));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameRenderPost()
-{
+function gameRenderPost() {
     // called after objects are rendered
     // draw effects or hud that appear above all objects
     // drawTextScreen('Hello World!', mainCanvasSize.scale(.5), 80);
